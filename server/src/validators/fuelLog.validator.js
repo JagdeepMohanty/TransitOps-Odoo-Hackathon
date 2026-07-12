a52568513d54f1692659f60deafb7297c6ea7543
lucky-panchal <luckykanti31122006@gmail.com>
2026-07-12 10:09:36 +0530

    feat(validator): add fuel log create schema

---
+++ server/src/validators/fuelLog.validator.js
@@ -0,0 +1,12 @@
+const { z } = require('zod');
+
+const createFuelLogSchema = z.object({
+  vehicleId: z.number().int().positive(),
+  tripId: z.number().int().positive().optional(),
+  liters: z.number().positive(),
+  cost: z.number().min(0),
+  odometer: z.number().min(0),
+  date: z.string().refine((val) => !isNaN(Date.parse(val)), { message: 'Invalid date' }),
+});
+
+module.exports = { createFuelLogSchema };
